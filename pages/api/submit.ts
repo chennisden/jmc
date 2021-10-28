8c98f57876ab5fb90de729f0b787d742246ac6ee
Dennis Chen <proofprogram@gmail.com>
2021-10-27 19:33:11 -0700

    Fix IP Log?

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -1,7 +1,7 @@
 import { jmcDB } from "@/utils/mongo";
 
 export default async (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
   const request = JSON.parse(req.body);
   await jmcDB.collection("submissions" + request.version).insertOne({
     username: request.username,
