3d737b31a3f1b01e3047ac6a4a4ed0661b59ebe0
Dennis Chen <proofprogram@gmail.com>
2021-10-27 19:44:56 -0700

    Cookies so auth is less annoying

--- pages/api/admin-verify.ts
+++ pages/api/admin-verify.ts
@@ -1,7 +1,17 @@
+import Cookies from "cookies";
+
 export default async (req, res) => {
-    if (process.env.ADMIN_PWD === req.body) {
-        res.status(200).send("Successfully authenticated.")
-    } else {
-        res.status(400).send("Wrong password.")
-    }
+  const cookies = new Cookies(req, res, { secure: true });
+  const password = cookies.get("password");
+  if (password && password === process.env.ADMIN_PWD) {
+    res.status(200).send("Successfully authenticated.");
+    return;
+  }
+  if (process.env.ADMIN_PWD === req.body) {
+    res.status(200).send("Successfully authenticated.");
+    cookies.set("password", req.body);
+    return;
+  }
+  res.status(400).send("Wrong password.");
+  return;
 };
