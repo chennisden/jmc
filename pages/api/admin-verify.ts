180380a52591149812b93607eb326e420d8b0020
Dennis Chen <proofprogram@gmail.com>
2021-10-27 20:06:55 -0700

    actually fix shit

--- pages/api/admin-verify.ts
+++ pages/api/admin-verify.ts
@@ -1,17 +1,26 @@
 import Cookies from "cookies";
 
 export default async (req, res) => {
-  const cookies = new Cookies(req, res, { secure: true });
-  const password = cookies.get("password");
-  if (password && password === process.env.ADMIN_PWD) {
-    res.status(200).send("Successfully authenticated.");
+  const request = JSON.parse(req.body);
+  const cookies = new Cookies(req, res);
+  if (request.action === "cookie") {
+    const password = cookies.get("password");
+    if (password && password === process.env.ADMIN_PWD) {
+      res.status(200).send("Successfully authenticated.");
+      return;
+    }
+    res.status(400).send("No cookie or wrong cookie");
     return;
   }
-  if (process.env.ADMIN_PWD === req.body) {
-    res.status(200).send("Successfully authenticated.");
-    cookies.set("password", req.body);
+  if (request.action === "set") {
+    if (process.env.ADMIN_PWD === request.password) {
+      cookies.set("password", request.password, { path: "/" });
+      res.status(200).send("Successfully authenticated.");
+      return;
+    }
+    res.status(400).send("Wrong password.");
     return;
   }
-  res.status(400).send("Wrong password.");
+  res.status(500).send("Invalid operation.");
   return;
 };
